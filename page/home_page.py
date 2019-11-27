446ccf036c7564dd358c190f25a40b1afee4934e
Administrator <1304210716@qq.com>
2019-11-27 10:56:11 +0800

    页面元素

---
+++ page/home_page.py
@@ -0,0 +1,8 @@
+"""
+主页页面
+"""
+from base.base_page import BasePage
+
+
+class HomePage(BasePage):
+    pass
---
+++ page/mine_page.py
@@ -0,0 +1,8 @@
+"""
+我的页面
+"""
+from base.base_page import BasePage
+
+
+class MinePage(BasePage):
+    pass
