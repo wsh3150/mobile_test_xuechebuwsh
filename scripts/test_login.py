2d1f8b1b25e803ed9a3ff16c78a581a612c27f4d
Administrator <1304210716@qq.com>
2019-11-27 14:48:22 +0800

    参数化正向测试用例

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -1,6 +1,8 @@
 """
 登录测试用例
 """
+import pytest
+
 from common.utils import init_driver
 from page.page_factory import PageFactory
 
@@ -16,14 +18,15 @@ class TestLogin(object):
     def teardown_class(self):
         """退出驱动对象"""
 
-    def test_login(self):
+    @pytest.mark.parametrize('name,pwd,expect', [('15535853634', 'zxc727397', '6547')])
+    def test_login(self, name, pwd, expect):
         """登录测试方法"""
         self.page_factory.home_page.click_mine()  # 点击我的
         self.page_factory.mine_page.click_login()  # 点击登录/注册
-        self.page_factory.login_page.input_username('15535853634')  # 输入账号
-        self.page_factory.login_page.input_password('zxc727397')  # 输入密码
+        self.page_factory.login_page.input_username(name)  # 输入账号
+        self.page_factory.login_page.input_password(pwd)  # 输入密码
         self.page_factory.login_page.click_login_btn()  # 点击登录按钮
         self.page_factory.login_page.click_con_btn()  # 点击签到确定按钮
         nick_name = self.page_factory.login_page.get_nick_name()  # 获取昵称
         print('昵称是', nick_name)
-        assert '6547' in nick_name  # 断言判断结果
+        assert expect in nick_name  # 断言判断结果
