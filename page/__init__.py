d40a3685939ac7b4b6f480812341aca639ac15da
Administrator <1304210716@qq.com>
2019-11-27 10:49:10 +0800

    PO元素属性及定位方法

---
+++ page/__init__.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.common.by import By
+
+# 主页页面
+mine = By.XPATH, '//*[contains(@text,"我的")]'  # 我的
+
+# 我的页面
+login = By.XPATH, '//*[contains(@text,"登录")]'  # 登录
+
+# 登录页面
+username = By.ID, 'com.bjcsxq.chat.carfriend:id/login_phone_et'  # 账号
+password = By.ID, 'com.bjcsxq.chat.carfriend:id/login_pwd_et'  # 密码
+login_btn = By.ID, 'com.bjcsxq.chat.carfriend:id/login_btn'  # 登录
+con_btn = By.XPATH, '//*[contains(@text,"确定")]'  # 签到提示框确认按钮
+nick_name = By.ID, 'com.bjcsxq.chat.carfriend:id/mine_username_tv'  # 昵称
